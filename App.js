2d82b56221f05cc4250c42f7f5fc6c637bf8e21c
Arkaraj <arkaraj2017@gmail.com>
2021-03-17 23:56:18 +0530

    Native dev 2

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, View, StyleSheet, StatusBar as SB } from 'react-native';
 
 export default function App() {
+  // Development
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+
+      <View style={styles.container}>
+
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }} />
+        <View style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          alignSelf: "flex-start"
+        }} />
+        <View style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }} />
+
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row", // horizontal
+    justifyContent: "center", // main
+    alignItems: "center", // secondary
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? SB.currentHeight : 0
   },
 });
